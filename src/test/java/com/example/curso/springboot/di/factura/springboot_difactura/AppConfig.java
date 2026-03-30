f022a082ad8f3a055ec7a109b8cd03cb1a711da2
Figue <alvaro.figueroa3206@gmail.com>
2026-03-30 16:47:51 +0200

    Cosillas

---
+++ src/test/java/com/example/curso/springboot/di/factura/springboot_difactura/AppConfig.java
@@ -0,0 +1,26 @@
+package com.example.curso.springboot.di.factura.springboot_difactura;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.example.curso.springboot.di.factura.springboot_difactura.models.Item;
+import com.example.curso.springboot.di.factura.springboot_difactura.models.Product;
+
+@Configuration
+@PropertySource("classpath:data.properties")
+public class AppConfig {
+
+    @Bean
+    List<Item> itemsInvoice(){
+        Product p1 = new Product("Camara sony", 800);
+        Product p2 = new Product("Bicicleta Bianchi 26", 1200);
+        List<Item> items = Arrays.asList(new Item(p1, 2), new Item(p2, 3));
+
+        return items;
+    }    
+
+}
--- src/test/java/com/example/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
+++ src/test/java/com/example/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
@@ -3,6 +3,7 @@ package com.example.curso.springboot.di.factura.springboot_difactura.models;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,9 @@ public class Invoice {
 
     @Autowired
     private Client client;
+    @Value("${invoice.description}")
     private String description;
+    @Autowired
     private List<Item> items;
 
     public Client getClient() {
