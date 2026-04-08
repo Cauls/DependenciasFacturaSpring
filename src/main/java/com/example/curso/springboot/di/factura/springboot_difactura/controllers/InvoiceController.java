6bbb34250b690a0f02b59fe735c219aaa615c47e
Figue <alvaro.figueroa3206@gmail.com>
2026-04-08 16:30:49 +0200

    Final de la factura

--- src/main/java/com/example/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
+++ src/main/java/com/example/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.curso.springboot.di.factura.springboot_difactura.models.Client;
 import com.example.curso.springboot.di.factura.springboot_difactura.models.Invoice;
 
 @RestController
@@ -16,6 +17,13 @@ public class InvoiceController {
 
     @GetMapping("/show")
     public Invoice show(){
-        return invoice;
+        Invoice i = new Invoice();
+        Client c = new Client();
+        c.setLastname(invoice.getClient().getLastname());
+        c.setName(invoice.getClient().getName());
+        i.setClient(c);
+        i.setDescription(invoice.getDescription());
+        i.setItemsInvoice(invoice.getItemsInvoice());
+        return i;
     }
 }
--- src/main/java/com/example/curso/springboot/di/factura/springboot_difactura/models/Client.java
+++ src/main/java/com/example/curso/springboot/di/factura/springboot_difactura/models/Client.java
@@ -2,8 +2,13 @@ package com.example.curso.springboot.di.factura.springboot_difactura.models;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import com.fasterxml.jackson.annotation.JsonSerializeAs;
 
 @Component
+@RequestScope
+@JsonSerializeAs(Client.class)
 public class Client {
 
     @Value("${client.name}")
